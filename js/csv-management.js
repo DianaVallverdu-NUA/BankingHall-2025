9b90c32396b5bdcc446ced8c90439663f543e1d7
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-12-11 12:42:27 +0000

    get info on specific year & area & draw square

--- js/csv-management.js
+++ js/csv-management.js
@@ -15,7 +15,6 @@ function parseCSV(data) {
   const result = rows.map((row) => row.split(","));
   //set info to result
   draughtInformation = result;
-  console.log(draughtInformation);
 }
 
 /**
@@ -31,6 +30,30 @@ async function fetchCSV(fileUrl) {
 
   const text = await response.text();
   parseCSV(text);
+}
+
+/**
+ * get all rows that are linked to this geocode in the CSV
+ * @param {*} geoCode 
+ */
+function getRowsFromArea(geoCode) {
+  let rows = [];
+  for(let row of draughtInformation) {
+    if(row[5] === geoCode) rows.push(row);
+  }
+  return rows;
+}
+
+/**
+ * get single row given a year and an array of rows
+ * @param {Array} rows list of rows to check
+ * @param {Number} year year from 2000 to 2023
+ * @returns 
+ */
+function getRowFromYear(rows, year) {
+  for(let row of rows) {
+    if(row[7] == year) return row;
+  }
 }
 
 fetchCSV(fileUrl);
--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
-let geoCode = "EU27_2020";
-
-
+let geoCode = "BE";
+let year = 2011;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
   background(220);
+
+  //get rows in current area
+  let areaRows = getRowsFromArea(geoCode);
+
+  //get rows in current year
+  let rowFromYear = getRowFromYear(areaRows, year);
+
+  let area = Number(rowFromYear[8]);
+
+  console.log(area);
+
+  fill(200, 100, 0);
+  square(windowWidth * 0.5 - area * 0.5, windowWidth * 0.5 - area * 0.5, area);
 }
